525ecf2c0746e35fa5ecb162f18a61c692d89460
Phillip Vo <pvo4@mail.valenciacollege.edu>
2016-03-16 12:23:44 -0400

    Week 10: 1st Attempt

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -29,5 +29,36 @@ capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix',
             #       2. instead of creating quiz and answer files in the current working directory, create a folder titled 'quizzes' and another folder titled 'answers'.
             #       3. place the randomly-generated quizzes in the 'quizzes' directory.
             #       4. plaec the corresponding answers in the 'answers' directory.
-            
-            
+
+os.makedirs('.\\answers')
+os.makedirs('.\\quizzes')
+
+# Generate 5 quiz files.
+for quizNum in range(5):
+    quizFile = open('.\\quizzes\\capitalsquiz%s.txt' % (quizNum + 1), 'w')
+    answerKeyFile = open('.\\answers\\capitalsquiz_answers%s.txt' % (quizNum + 1), 'w')
+    quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+    quizFile.write((' ' * 20) + 'State Capitals Quiz (Form %s)' % (quizNum + 1))
+    quizFile.write('\n\n')
+    states = list(capitals.keys())
+    random.shuffle(states)           
+
+
+    # Loop through all 50 states, making a question for each.
+    for questionNum in range(50):
+        correctAnswer = capitals[states[questionNum]]
+        wrongAnswers = list(capitals.values())
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        wrongAnswers = random.sample(wrongAnswers, 3)
+        answerOptions = wrongAnswers + [correctAnswer]
+        random.shuffle(answerOptions)
+
+
+        # Write the question and the answer options to the quiz file.
+        quizFile.write('%s. What is the capital of %s?\n' % (questionNum + 1, states[questionNum]))
+        for i in range(4):
+            quizFile.write(' %s. %s\n' % ('ABCD'[i], answerOptions[i]))
+        quizFile.write('\n')
+        answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[answerOptions.index(correctAnswer)]))
+    quizFile.close()
+    answerKeyFile.close()
